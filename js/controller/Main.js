fa2f8103696cdce7abea273ccf06ea2028a7b19e
Guirerume <gpaula@2mundos.net>
2017-02-22 23:27:10 -0300

    Create Score

--- js/controller/Main.js
+++ js/controller/Main.js
@@ -3,7 +3,7 @@ var ctx = canvas.getContext("2d");
 
 var ball = new Ball(canvas.width / 2 - 6, canvas.height - 51, 12, 12, 1);
 var player = new Player(canvas.width /2 - 50, canvas.height - 30, 20, 100, 5);
-
+var score = new Score(10, 10, 0);
 var game = new Game(ball, player);
 
 setInterval(game.play, 10);
